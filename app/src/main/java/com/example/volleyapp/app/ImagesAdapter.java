fa57ddc0eaaf2278a406c828be1b2a0e7ca2e367
Chris Blunt <chris@plymouthsoftware.com>
2014-05-07 12:33:35 +0100

    Update ListView to show images.

--- app/src/main/java/com/example/volleyapp/app/ImagesAdapter.java
+++ app/src/main/java/com/example/volleyapp/app/ImagesAdapter.java
@@ -1,18 +1,23 @@
 package com.example.volleyapp.app;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.widget.ImageView;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
-* Created by chris on 07/05/2014.
-*/
+ * Created by chris on 07/05/2014.
+ */
 class ImagesAdapter extends BaseAdapter {
     private List<String> mUrls;
     private final Context mContext;
@@ -49,22 +54,36 @@ class ImagesAdapter extends BaseAdapter {
         View view = convertView;
 
         if(view == null) {
-            view = LayoutInflater.from(mContext).inflate(android.R.layout.simple_list_item_1, null);
+            view = LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
         }
 
         ViewHolder holder = (ViewHolder) view.getTag();
         if(holder == null) {
             holder = new ViewHolder();
-            holder.text1 = (TextView) view.findViewById(android.R.id.text1);
+            holder.image1 = (ImageView) view.findViewById(R.id.image1);
             view.setTag(holder);
         }
 
-        holder.text1.setText(getItem(position).toString());
+        final WeakReference<ViewHolder> holderWeakReference = new WeakReference<>(holder);
+        String url = (String) getItem(position);
+        ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap bitmap) {
+                ViewHolder weakHolder = holderWeakReference.get();
+
+                if(weakHolder != null) {
+                    weakHolder.image1.setImageBitmap(bitmap);
+                }
+            }
+        }, 0, 0, null, null);
+
+        VolleyApplication.getInstance().getRequestQueue().add(imageRequest);
+        holder.image1.setImageResource(R.drawable.ic_launcher);
 
         return view;
     }
 
     private class ViewHolder {
-        TextView text1;
+        ImageView image1;
     }
 }
